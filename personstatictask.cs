81e778bd9f5f42382daa24b307b17bf4cafcd541
KonulHuseynli <konulrh@code.edu.az>
2022-05-31 11:20:01 +0400

    added new task

---
+++ personstatictask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp64
+{
+    class Person
+    {
+        public string Name;
+        public  static int Age;
+        public void count()
+            {
+            Age++;
+        }
+        public int getAge()
+        {
+            return Age;
+        }
+
+    }
+}
---
+++ programstatictask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person konul = new Person();
+            konul.Name = "konul";
+           
+            Person Aysun = new Person();
+            konul.Name = "Aysun";
+
+            Person Mirsaleh = new Person();
+            konul.Name = "Mirsaleh";
+
+            Person Nurlan = new Person();
+            konul.Name = "Nurlan";
+
+            konul.count();
+            Console.WriteLine(konul.getAge());
+            Aysun.count();
+            Console.WriteLine(Aysun.getAge());
+            Mirsaleh.count();
+            Console.WriteLine(Mirsaleh.getAge());
+            Nurlan.count();
+            Console.WriteLine(Nurlan.getAge());
+        }
+    }
+}
